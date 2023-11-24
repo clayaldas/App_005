7179e0efaf901f465c026aab2500d02f87530d01
Clay Aldas <clayaldas@gmail.com>
2023-11-24 16:20:14 -0500

    Finalizada clase viernes 23 de noviembre

---
+++ app/src/main/java/uta/fisei/app_005/GuidesActivity.java
@@ -0,0 +1,14 @@
+package uta.fisei.app_005;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GuidesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guides);
+    }
+}
